e945db8e4e9e38dac244c0e19ce2cd881e8c3568
Iván López <lopez.ivan@gmail.com>
2013-12-16 11:09:58 +0100

    Add plugins and dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,19 +7,19 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+// grails.project.fork = [
+//     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+//     //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-]
+//     // configure settings for the test-app JVM, uses the daemon by default
+//     test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+//     // configure settings for the run-app JVM
+//     run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+//     // configure settings for the run-war JVM
+//     war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+//     // configure settings for the Console UI JVM
+//     console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+// ]
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
@@ -44,11 +44,15 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://repo.spring.io/milestone"
     }
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.24'
+        runtime "org.postgresql:postgresql:9.2-1004-jdbc4"
+
+        compile 'org.springframework.integration:spring-integration-core:3.0.0.RC1'
     }
 
     plugins {
@@ -68,5 +72,11 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
+        compile ":build-test-data:2.0.9"
+
+        compile ":postgresql-extensions:0.6.1"
+
+        compile ":spring-security-core:2.0-RC2"
+        compile ":spring-security-bcrypt:0.3"
     }
 }
