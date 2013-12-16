05c8ff506661e85b428c457e853f1188b6131167
Iván López <lopez.ivan@gmail.com>
2013-12-16 11:11:43 +0100

    Add some test data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,28 @@
+import yasn.user.Role
+import yasn.user.User
+import yasn.user.UserRole
+
+import yasn.relation.FollowRelation
+
 class BootStrap {
 
     def init = { servletContext ->
+        this.createDummyData()
     }
+
     def destroy = {
+    }
+
+    private createDummyData() {
+        def role = new Role(authority: 'ROLE_USER').save(flush: true, failOnError: true)
+        def user = new User(username: 'user', password: 'pass').save(flush: true, failOnError: true)
+        UserRole.create(user, role)
+
+        10.times { n ->
+            def tmpUser = new User(username: "user${n}", password: 'pass').save(flush: true, failOnError: true)
+            UserRole.create(tmpUser, role)
+
+            def fr = new FollowRelation(user: user, follower: tmpUser).save(flush: true, failOnError: true)
+        }
     }
 }
