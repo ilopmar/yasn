72abd553f90dc1d49736f112ad3c608e9214c5f8
Iván López <lopez.ivan@gmail.com>
2013-12-16 11:11:11 +0100

    Add database connection configuration

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -16,7 +16,12 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            driverClassName="org.postgresql.Driver"
+            dialect="net.kaleidos.hibernate.PostgresqlExtensionsDialect"
+            url = "jdbc:postgresql://localhost:5432/yasn"
+            username = "yasn"
+            password = "yasn"
+            logSql = true
         }
     }
     test {
