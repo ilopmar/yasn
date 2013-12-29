f9651401d475a5ec95363db11dc82c03d8228be7
Iván López <lopez.ivan@gmail.com>
2013-12-29 15:25:02 +0100

    Test to update a follower from SI flow

--- test/integration/yasn/relation/RelationServiceIntegrationSpec.groovy
+++ test/integration/yasn/relation/RelationServiceIntegrationSpec.groovy
@@ -4,6 +4,7 @@ import spock.lang.*
 
 import yasn.ro.FollowRequest
 import yasn.user.User
+import yasn.YasnRedisService
 
 class RelationServiceIntegrationSpec extends Specification {
 
@@ -45,6 +46,23 @@ class RelationServiceIntegrationSpec extends Specification {
 
         then:
             FollowRelation.countByFollowerAndUser(follower, user) == 1
+    }
+
+    void 'update the followers with the data of the follower flow'() {
+        setup:
+            def user = User.build()
+            def follower = User.build()
+            def followRequest = new FollowRequest(follower: follower, user: user)
+
+        and: 'mock collaborator'
+            def yasnRedisService = Mock(YasnRedisService)
+            relationService.yasnRedisService = yasnRedisService
+
+        when:
+            relationService.doUpdateFollowers(followRequest)
+
+        then:
+            1 * yasnRedisService.addFollower(follower, user)
     }
 
 }
